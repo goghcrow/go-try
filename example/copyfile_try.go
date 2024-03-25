9fdda49863a3db1f48264e6115b5eec517eb9df1
xiao <>
2024-03-26 04:21:27 +0800

    update readme

--- example/copyfile_try.go
+++ example/copyfile_try.go
@@ -2,17 +2,24 @@
 
 //go:generate go install github.com/goghcrow/go-try/cmd/trygen@main
 //go:generate trygen
+
 package example
 
 import (
+	"fmt"
 	"io"
 	"os"
 
 	. "github.com/goghcrow/go-try"
 )
 
+// CopyFile example
+// from https://github.com/golang/proposal/blob/master/design/32437-try-builtin.md#examples
+//
 //goland:noinspection GoUnhandledErrorResult
 func CopyFile(src, dst string) (err error) {
+	defer HandleErrorf(&err, "copy %s %s", src, dst)
+
 	r := Try(os.Open(src))
 	defer r.Close()
 
@@ -27,4 +34,10 @@ func CopyFile(src, dst string) (err error) {
 	Try(io.Copy(w, r))
 	Try0(w.Close())
 	return nil
+}
+
+func HandleErrorf(err *error, format string, args ...any) {
+	if *err != nil {
+		*err = fmt.Errorf(format+": %v", append(args, *err)...)
+	}
 }
